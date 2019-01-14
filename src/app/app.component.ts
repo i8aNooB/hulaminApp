0781040581b7555267cf8c6ff55682edc90f839d
HulaminApp <hulamin.app@gmail.com>
2019-01-14 00:55:36 -0500

    Menu Cleanup and Product Categories

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,91 +25,27 @@ export class MyApp {
         {
           title: 'Layout with firebase',
           myicon:'',
-          iconLeft: 'ios-filing',
+          iconLeft: 'ios-contact',
           icon: 'ios-add-outline',
           showDetails: false,
           items:  [
-                
-              {name:'Authentication(Login)',component:'MainPage'},
-              {name:'Authentication(Register)',component:'RegisterPage'},
-              {name:'Authentication(Forgot)',component:'ForgotPage'},
-              {name:'Authentication(Profile)',component:'AfterLoginPage'},
-              {name:'CRUD(create,read,update,delete)',component: 'FormResultPage'},
-              {name:'Chart',component:'ChartPage'},
-
-              {name:'City guide', component: 'Category1Page'},// app1 folder
-              {name:'Shopping',component:'Category2Page'},// app2 folder
-              {name:'Restaurant',component:'Category3Page'}, // app3 folder
-              {name:'Google map',component: 'MapPage'},
-              {name:'Image gallery',component: 'GalleryPage'},
-              {name:'Feed',component: 'FeedPage'},
-           
-              {name:'Intro', component:'IntroPage'},
-
-              {name:'Pinterest(Masonry)',component: 'MasonryPage'},
-              {name:'Profile1',component: 'ProfilePage'},
-              {name:'Profile2',component: 'Profile2Page'},
-              {name:'Profile3',component: 'Profile3Page'},
-              {name:'Profile4', component: 'Profile4Page'},
-              {name:'Radio Station',component:'RadioListPage'},
-
-              {name:'Search',component:'SearchPage'},
-              {name:'Timeline',component: 'TimelinePage'}
+              {name:'My Profile',component:'AfterLoginPage'},
           ]
         }, {
-          title: 'Components',
-          iconLeft: 'ios-copy',
+          title: 'Layout with firebase',
+          myicon:'',
+          iconLeft: 'ios-list-box',
           icon: 'ios-add-outline',
           showDetails: false,
           items:  [
-                {name:'Accordion',component:'AccordionPage'},
-
-                {name:'Action sheet',component:'ActionsheetPage'},
-                {name:'Alert',component:'AlertPage'},
-                {name:'Animation',component:'AnimationsPage'},
-
-                {name:'Button',component:'ButtonPage'},
-                {name:'Datetime',component:'DatetimePage'},
-                {name:'Fab', component:'FabPage'},
-                {name:'Fading header',component:'FadingHeaderPage' },
-                {name:'Grid', component:'GridPage'},
-                {name:'Header',component:'HeaderPage'},
-                {name:'Input',component:'InputPage'},
-                {name:'Item',component:'ItemPage'},
-                {name:'Item sliding',component:'ItemSlidingPage'},
-                {name:'Label',component:'LabelPage'},
-                {name:'Radio button',component:'RadioButtonPage'},
-                {name:'Rating',component:'RatingPage'},
-                
-                {name:'Range',component:'RangePage'},
-                {name:'Search bar', component:'SearchBarPage'},
-                {name:'Select option',component:'SelectOptionPage'},
-                {name:'Segment',component:'SegmentPage'},
-                {name:'Shrinking',component:'ShrinkingPage'},
-
-                {name:'Tag',component:'TagPage'},
-                {name:'Table',component:'TablePage'},
-                {name:'Transparent header',component:'TransparentHeaderPage'},
-                {name:'Toast',component:'ToastPage'}
-
-            ]
-        },{
-          title: 'Theme',
-          iconLeft: 'md-color-palette',
-          icon: 'ios-add-outline',
-          showDetails: false,
-          items:  [
-            {
-            name:'Color',
-            component:'ThemePage'
-            }
+              {name:'Products Catalog',component:'Category2Page'},// app2 folder
           ]
         }
     ];
 
     this.pages = [ 
       // { icon:'call', title:'Contact us', component: 'ContactPage' },
-      { icon:'bookmark', title:'Version 3.0', component: "MainPage" }    
+      { icon:'exit', title:'Logout', component: "MainPage" }    
     ];
 
   }
